d6cbbadf709e695b8c1912bb86a0a06652253da4
Lei <dongleivip@gmail.com>
2019-05-15 13:21:34 +0800

    change implementation to for

--- Fibonacci/FibonacciGenerator.cs
+++ Fibonacci/FibonacciGenerator.cs
@@ -5,19 +5,42 @@ namespace Fibonacci
     public class FibonacciGenerator
     {
         // F(n) = F(n-1) + F(n-2)
-        public int GenerateFibonacci(int number)
+//        public long GenerateFibonacci(int number)
+//        {
+//            if (number == 0 || number == 1)
+//            {
+//                return 1;
+//            }
+//
+//            var priorFirst = GenerateFibonacci(number - 1);
+//            var priorSecond = GenerateFibonacci(number - 2);
+//            
+//            var result = priorFirst + priorSecond;
+//
+//            return result;
+//        }
+        
+        
+        public long GenerateFibonacci(int number)
         {
             if (number == 0 || number == 1)
             {
                 return 1;
             }
-
-            var priorFirst = GenerateFibonacci(number - 1);
-            var priorSecond = GenerateFibonacci(number - 2);
             
-            var result = priorFirst + priorSecond;
+            long fibNMinusOne = 1;
+            long fibNMinusTwo = 1;
+            long fibN = 0;
 
-            return result;
+            for (var i = 2; i <= number; i++)
+            {
+                fibN = fibNMinusOne + fibNMinusTwo;
+
+                fibNMinusTwo = fibNMinusOne;
+                fibNMinusOne = fibN;
+            }
+
+            return fibN;
         }
     }
 }
