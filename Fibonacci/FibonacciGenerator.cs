8f5638d331294b2754e5e958079638114de6f5b8
Lei <dongleivip@gmail.com>
2019-05-15 12:46:15 +0800

    Fibonacci by recursion

--- Fibonacci/FibonacciGenerator.cs
+++ Fibonacci/FibonacciGenerator.cs
@@ -4,9 +4,20 @@ namespace Fibonacci
 {
     public class FibonacciGenerator
     {
+        // F(n) = F(n-1) + F(n-2)
         public int GenerateFibonacci(int number)
         {
-            throw new NotImplementedException();
+            if (number == 0 || number == 1)
+            {
+                return 1;
+            }
+
+            var priorFirst = GenerateFibonacci(number - 1);
+            var priorSecond = GenerateFibonacci(number - 2);
+            
+            var result = priorFirst + priorSecond;
+
+            return result;
         }
     }
 }
--- UnitTests/FibonacciGeneratorTests.cs
+++ UnitTests/FibonacciGeneratorTests.cs
@@ -13,11 +13,29 @@ namespace UnitTests
         }
         
         [Fact]
+        public void ShouldReturnOneWhenInputZero()
+        {
+            var fibonacci = _fibonacciGenerator.GenerateFibonacci(0);
+            
+            Assert.Equal(1, fibonacci);
+        }
+
+        [Fact]
         public void ShouldReturnOneWhenInputOne()
         {
             var fibonacci = _fibonacciGenerator.GenerateFibonacci(1);
             
             Assert.Equal(1, fibonacci);
+        }
+
+        [Theory]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        public void ShouldReturnTheSumOfTheTwoPriorNumberWHenInputN(int inputNumber, int expected)
+        {
+            var fibonacci = _fibonacciGenerator.GenerateFibonacci(inputNumber);
+            
+            Assert.Equal(expected, fibonacci);
         }
     }
 }
